85b12604e3c29f5813b3ad23a56f94314e89a84d
Kartik Arora <ka340689@gmail.com>
2022-11-18 09:27:11 +0000

    deploy

--- vercel-python-app/index.py
+++ vercel-python-app/index.py
@@ -19,7 +19,7 @@ def home():
     subject='Booking Enquiry by '+data["password"] +" Email: "+data["email"],
     html_content='<strong>'+data["startTime"]+"<br>"+data["endTime"]+"<br>"+data["date"]+"<br>"+data["message"]+"<strong>")
 
-    sg = SendGridAPIClient("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")
+    sg = SendGridAPIClient("os.getenv('SENDGRID_API_KEY')")
     response = sg.send(message)
     return jsonify("Mail Sent Successfully")
 
